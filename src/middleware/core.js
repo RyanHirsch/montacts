a9601ddbc9f3ca88a1e68c2ff0571599fcee0887
Ryan Hirsch <ryan.hirsch@gmail.com>
2016-12-28 10:26:21 -0600

    add cors support

--- src/middleware/core.js
+++ src/middleware/core.js
@@ -1,9 +1,11 @@
 import { name } from '../utils/logger';
 import expressBunyanLogger from 'express-bunyan-logger';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 export default function(app) {
   app.use(expressBunyanLogger({ name }));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(cors());
 }
